967263e4c905fb5a9114e720fefb0f6fd14103f9
Sanjay Singh Shahi <shahi.sanjayshahi@gmail.com>
2026-01-17 14:50:27 +0530

    Production ready changes

--- src/config/env.js
+++ src/config/env.js
@@ -2,21 +2,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const requiredEnvVars = [
-  "PORT",
-  "MONGO_URI",
-  "JWT_ACCESS_SECRET",
-  "JWT_REFRESH_SECRET",
-];
-
-requiredEnvVars.forEach((key) => {
-  if (!process.env[key]) {
-    throw new Error(`Missing required env variable: ${key}`);
-  }
-});
+["PORT", "MONGO_URI", "JWT_ACCESS_SECRET", "JWT_REFRESH_SECRET"].forEach(
+  (key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing env: ${key}`);
+    }
+  },
+);
 
 export const env = {
-  port: process.env.PORT,
+  port: Number(process.env.PORT) || 5000,
   mongoUri: process.env.MONGO_URI,
   nodeEnv: process.env.NODE_ENV || "development",
   jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
